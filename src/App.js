ba3ad6c48d9b29dee3ccf19a580cc72cbdcfbb79
Jimmy Solis <jimmysolis14@gmail.com>
2023-05-09 04:34:32 -0400

    MAde alot of steps

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import './product.css';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
@@ -5,14 +6,18 @@ import Home from './pages/home';
 import Product from './pages/product';
 
 function App() {
+
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/product" component={Product} />
-      </Switch> 
-    </Router>
-  );
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/product" component={Product} />
+        </Switch> 
+      </Router>
+  )
+ 
 }
 
 export default App;
+
+
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,47 +1,73 @@
 import React, { useState, useEffect } from 'react';
-
+import { Link } from "react-router-dom";
+import bat from "../img/bat.gif"
+import logo from "../img/logo.png"
 
 const images = [
-    { src: 'https://via.placeholder.com/300x300', alt: 'Image 1' },
-    { src: 'https://via.placeholder.com/300x300', alt: 'Image 2' },
-    { src: 'https://via.placeholder.com/300x300', alt: 'Image 3' },
-    { src: 'https://via.placeholder.com/300x300', alt: 'Image 4' },
-    { src: 'https://via.placeholder.com/300x300', alt: 'Image 5' },
-    { src: 'https://via.placeholder.com/300x300', alt: 'Image 6' },
-  ];
+  { src: bat, alt: 'Image 1' },
+  { src: bat, alt: 'Image 2' },
+  { src: bat, alt: 'Image 3' },
+  { src: bat, alt: 'Image 4' },
+  { src: bat, alt: 'Image 5' },
+];
 
-  export default function Home () {
-    const [isHeaderShrunk, setIsHeaderShrunk] = useState(false);
+const bgImages = [
+  'https://i.pinimg.com/originals/53/bd/f3/53bdf3c62c768c92df672bb06d97df90.gif',
+  'https://media4.giphy.com/media/3o7aCZFOhEO4b23Cfu/giphy.gif',
+  'https://media.tenor.com/oXvT_cWqiqcAAAAC/vandalism-graffitibanksy.gif',
+  "https://64.media.tumblr.com/09df1ce9f5e9d190ed38d69ec62927fe/43491dbbd5b89ec0-e8/s500x750/5a4d5bda47834af7f94d80113b73575f0f76a7e0.gifv",
+  "https://media2.giphy.com/media/rmtb3GnVZAwWA/giphy.gif"
+];
 
-    useEffect(() => {
-      const handleScroll = () => {
-        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        const newScrollTop = scrollTop + 900
-        const headerHeight = document.querySelector('.App-header').clientHeight;
-        setIsHeaderShrunk(newScrollTop > headerHeight);
-        console.log(newScrollTop)
-      };
-  
-      window.addEventListener('scroll', handleScroll);
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-  
-    const headerStyle = isHeaderShrunk ? { height: '100px', position: 'sticky', transition:'3s'} : { height: '1000px' };
-  
-    return (
-      <div className="App">
-        <header className="App-header" style={headerStyle}>
-          <h1>James Geovanny</h1>
-        </header>
-        <div className="image-grid">
-          {images.map((image) => (
-            <img key={image.alt} src={image.src} alt={image.alt} className='img-individual' />
-          ))}
-        </div>
+export default function Home() {
+  const relocate = () => {
+    console.log('clicked');
+    window.location.href = '/product';
+  };
+
+  const [isHeaderShrunk, setIsHeaderShrunk] = useState(false);
+  const [bgIndex, setBgIndex] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      const newScrollTop = scrollTop + 950;
+
+      const headerHeight = document.querySelector('.App-header').clientHeight;
+      setIsHeaderShrunk(newScrollTop > headerHeight);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setBgIndex(prevIndex => (prevIndex + 1) % bgImages.length);
+    }, 5000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const headerStyle = {
+    ...isHeaderShrunk ? { height: '150px', position: 'sticky', transition: '3s' } : { height: '1000px' },
+    backgroundImage: `url(${bgImages[bgIndex]})`,
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header" style={headerStyle}>
+        <img src={logo} style={{ width: '40vmin', height: 'auto' }} />
+      </header>
+      <div className="image-grid">
+        {images.map((image) => (
+          <Link to="/product" onClick={relocate} key={image.alt}>
+            <img key={image.alt} src={image.src} alt={image.alt} className="img-individual" />
+          </Link>
+        ))}
       </div>
-    );
-  }
-
-  
+    </div>
+  );
+}
